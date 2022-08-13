8ae8c80dce9022d239501e1734c70ce55118c1f6
nvishwanatha <n.vishwanatha@gmail.com>
2022-08-13 13:00:41 -0700

    Initial commit

---
+++ src/main/java/com/techSpring/kafka/springbootkafkaproducerexample/resources/UserResource.java
@@ -0,0 +1,24 @@
+package com.techSpring.kafka.springbootkafkaproducerexample.resources;
+
+import com.techSpring.kafka.springbootkafkaproducerexample.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("kafka")
+public class UserResource {
+    @Autowired
+    KafkaTemplate<String, User> kafkaTemplate;
+    private static final String TOPIC = "kafka-Example";
+
+    @GetMapping("/publish/{name}")
+    public String post(@PathVariable("name") final String name) {
+        kafkaTemplate.send(TOPIC, new User(name, "Technology", 1200000L));
+        return "Published finally - first Code";
+    }
+
+}
